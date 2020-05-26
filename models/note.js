9fd3b4a580f0e808d38a28cc091b612919a841f2
zhanlinsong <zhanlscq@163.com>
2020-05-26 11:38:45 +0800

    加入mongoose-unique-validator中间件验证注册用户名唯一性

--- models/note.js
+++ models/note.js
@@ -5,6 +5,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
+mongoose.set('useCreateIndex', true)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(res => {
     console.log('connected to MongoDB at ' + res.connections[0].host + ":" + res.connections[0].port)
